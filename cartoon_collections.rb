b88d22d34b43f88a6bb9d93b935eb6a13a5c43db
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 00:29:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planeteer_calls)
   return planeteer_calls.map{|x| "#{x.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  return words.map{ |x| x.length>4}.includes?
 end
 
 def find_the_cheese# code an argument here
